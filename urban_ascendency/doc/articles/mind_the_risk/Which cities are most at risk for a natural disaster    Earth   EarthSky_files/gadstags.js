303528b0820f570e4d8a23b826cfa5262bc7bfa5
mkl <mkl48@nau.edu>
2014-02-18 16:17:10 -0500

    organization

---
+++ urban_ascendency/doc/articles/mind_the_risk/Which cities are most at risk for a natural disaster    Earth   EarthSky_files/gadstags.js
@@ -0,0 +1,65 @@
+googletag.cmd.push(function() {
+
+  // Kamoona
+  googletag.defineSlot('/12836845/Komoona', [300, 250], 'div-gpt-ad-1372960173900-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Homepage_Leaderboard
+  googletag.defineSlot('/12836845/Homepage_Leaderboard', [728, 90], 'div-gpt-ad-1329854618247-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Leaderboard - Right
+  googletag.defineSlot('/12836845/Right_of_Leaderboard', [217, 90],'div-gpt-ad-1342190704847-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Homepage_1stSidebarRect
+  googletag.defineSlot('/12836845/Homepage_1stSidebarRect', [300, 250], 'div-gpt-ad-1329854807364-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Homepage_2ndSidebarRect
+  googletag.defineSlot('/12836845/Homepage_2ndSidebarRect', [300, 250], 'div-gpt-ad-1329855599779-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Tonight_Leaderboard
+  googletag.defineSlot('/12836845/Tonight_Leaderboard', [728, 90], 'div-gpt-ad-1329856530570-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Tonight_1stSidebarRect
+  googletag.defineSlot('/12836845/Tonight_1stSidebarRect', [300, 250], 'div-gpt-ad-1329856560003-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // Tonight_2ndSidebarRect
+  googletag.defineSlot('/12836845/Tonight_2ndSidebarRect', [300, 250], 'div-gpt-ad-1329856772552-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // TonightLinkUnit
+  googletag.defineSlot('/12836845/TonightLinkUnit', [125, 90], 'div-gpt-ad-1329856827659-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // PostLinkUnit
+  googletag.defineSlot('/12836845/PostLinkUnit', [468, 15], 'div-gpt-ad-1336062812552-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // ROS_Leaderboard
+  googletag.defineSlot('/12836845/ROS_Leaderboard', [728, 90], 'div-gpt-ad-1329857672578-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // ROS_1stSidebarRect
+  googletag.defineSlot('/12836845/ROS_1stSidebarRect', [300, 250], 'div-gpt-ad-1329857616214-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // ROS_2ndSidebarRect
+  googletag.defineSlot('/12836845/ROS_2ndSidebarRect', [300, 250], 'div-gpt-ad-1329857710196-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  // BottomPostUnit
+  googletag.defineSlot('/12836845/BottomPostUnit', [468, 60], 'div-gpt-ad-1337959884206-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  //FTF Dow Logo
+  googletag.defineSlot('/12836845/FTF_DOW_LOGO', [105, 50], 'div-gpt-ad-1339693021506-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  //FTF Leaderboard
+  googletag.defineSlot('/12836845/FTF_728x90', [728, 90], 'div-gpt-ad-1339705812513-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  //FTF Sidebar
+  googletag.defineSlot('/12836845/FTF_300x250', [300, 250], 'div-gpt-ad-1339705766430-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  //FTF Cobranded
+  googletag.defineSlot('/12836845/co_branded_promo_unit', [300, 134], 'div-gpt-ad-1343066385690-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  //Post Bottom Left
+  googletag.defineSlot('/12836845/Left_BottomPostUnit', [117, 60], 'div-gpt-ad-1342454360182-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  //House 350x200
+  googletag.defineSlot('/12836845/House_300x250_NoAdsense', [300, 250], 'div-gpt-ad-1342639500863-0').addService(googletag.pubads().set("page_url", window.location.href));
+
+  googletag.pubads().enableSingleRequest();
+  googletag.enableServices();
+  });
